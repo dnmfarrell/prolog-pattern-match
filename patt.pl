55043aa5d2cf26f5f2a8f3d0efa47b04183ef795
David Farrell <1469333+dnmfarrell@users.noreply.github.com>
2023-08-03 09:23:08 -0400

    refine docs

--- patt.pl
+++ patt.pl
@@ -1,7 +1,6 @@
 % DCG to match strings to string patterns, e.g "foo" = "f*".
 
-% patt//1 is the interface to the DCG. It matches an empty string or a string
-% pattern.
+% patt//1 is the interface to the DCG.
 patt([])       --> [].
 patt([C|Cs])   --> char(C), patt(Cs).
 patt(['*'|Cs]) --> word, chrs(Cs).
@@ -10,10 +9,10 @@ patt(['*'|Cs]) --> word, chrs(Cs).
 chrs([])       --> [].
 chrs([C|Cs])   --> char(C), patt(Cs).
 
-% char//1 matches any printable character.
+% char//1 matches any printable character, and space.
 char(C)        --> [C], { char_type(C, graph) }.
 char(' ')      --> [' '].
 
-% word//0 is a terminal which generates sequences of printable characters.
+% word//0 generates sequences of char//1.
 word           --> char(_), (word | empt).
 empt           --> [].
